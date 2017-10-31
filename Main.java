4587b0e3a3021198a522fde70ca53d46d821ef46
Donghao Qiao <dqiao2@stevens.edu>
2017-10-30 22:04:20 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+package Roboat;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		new DashBoard();
+		new ControlPanel();
+		new Map();
+	}
+}
